edbd67cac7d3a96d56e7b73051d0c116d198424c
Hayata <hayata8812@gmail.com>
2020-11-24 18:42:07 +0900

    練習

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,129 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" color="#eefe" clipped app>
+      <v-container>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="title grey--text text--darken-2">
+              Navigation lists
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list nav dense>
+          <v-list-group
+          v-for="nav_list in nav_lists"
+          :key="nav_list.name"
+          :prepend-icon="nav_list.icon"
+          no-action
+          :append-icon="nav_list.lists ? undefined : ''">
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>{{ nav_list.name }}</v-list-item-title>
+              </v-list-item-content>
+            </template>
+            <v-list-item v-for="list in nav_list.lists" :key="list">
+              <v-list-item-content>
+                <v-list-item-title>{{ list }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+        </v-list>
+      </v-container>
+    </v-navigation-drawer>
+    <v-app-bar color="green" clipped-left dark app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Vuetify</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items>
+        <v-btn text>For Enterprise</v-btn>
+        <v-menu offset-y>
+          <template v-slot:activator="{on}">
+          <v-btn v-on="on" text>Support<v-icon>mdi-menu-down</v-icon></v-btn>
+          </template>
+          <v-list>
+            <v-subheader>Get help</v-subheader>
+            <v-list-item v-for="support in supports" :key="support">
+              <v-list-item-icon>
+                <v-icon>{{support.icon}}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>{{ support.name }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-toolbar-items>
+    </v-app-bar>
+
+    <v-main>
+      <v-form>
+        <v-text-field>
+          <input type="text">
+        </v-text-field>
+      </v-form>
+    </v-main>
+
+    <v-footer color="green" dark app>
+      Vuetify
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+
+  },
+
+  data(){
+    return{
+        drawer: null,
+        supports:[
+          {name: 'Consulting and suppourt',icon: 'mdi-vuetify'},
+          {name: 'Discord community',icon: 'mdi-discord'},
+          {name: 'Report a bug',icon: 'mdi-bug'},
+          {name: 'Github issue board',icon: 'mdi-github'},
+          {name: 'Stack overview',icon: 'mdi-stack-overflow'},
+        ],
+        nav_lists:[
+          {
+            name: 'Getting Started',
+            icon: 'mdi-speedometer',
+            lists:['Quick Start','Pre-made layouts']
+          },
+          {
+            name: 'Customization',
+            icon: 'mdi-cogs'
+          },
+          {
+            name: 'Styles & animations',
+            icon: 'mdi-palette',
+            lists:['Colors','Content','Display']
+          },
+          {
+            name: 'UI Components',
+            icon: 'mdi-view-dashboard',
+            lists:['API explorer','Alerts']
+          },
+          {
+            name: 'Directives',
+            icon: 'mdi-function'
+          },
+          {
+            name: 'Preminum themes',
+            icon: 'mdi-vuetify'
+          },
+        ]
+
+    }
+  }
+};
+</script>
